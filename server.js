58e5a2beac432958a11f42fbe737190b9cc44fde
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-02-09 16:09:49 +0800

    基礎設置

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import path from 'path'
+import mongoose from 'mongoose'
+
+dotenv.config()
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'src', 'views', 'index.html');
+
+mongoose.connect(`${process.env.MongoDB_url}`)
+  .then(() => {
+    console.log("Connection OPEN!!!")
+  })
+  .catch((err) => {
+    console.log("OH NOOOOO")
+    console.log(err)
+  })
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+// 引入頁面
+import LoginView from '../views/LoginView.vue'
+import AdminDashboard from '../views/admin/AdminDashboard.vue'
+import StaffOrder from '../views/staff/StaffOrder.vue'
+import CustomerMenu from '../views/customer/CustomerMenu.vue'
+
+// // 假設的檢查登入狀態函數
+// const isLoggedIn = () => {
+//   // 實際應該從你的登入狀態中判斷，比如檢查 Token 是否存在
+//   return localStorage.getItem('authToken') !== null
+// }
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/login',
+      name: 'login',
+      component: LoginView, // 登入頁面
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/admin',
+      name: 'admin',
+      component: AdminDashboard,
+      meta: { requiresAuth: true }, // 需要登入
+    },
+    {
+      path: '/staff',
+      name: 'staff',
+      component: StaffOrder,
+      meta: { requiresAuth: true }, // 需要登入
+    },
+    {
+      path: '/customer',
+      name: 'customer',
+      component: CustomerMenu,
     },
   ],
 })
+
+// // 全局前置守衛
+// router.beforeEach((to, from, next) => {
+//   // 如果路由需要登入，且未登入，則跳轉到 /login
+//   if (to.meta.requiresAuth && !isLoggedIn()) {
+//     next({ path: '/login' }) // 跳轉到登入頁面
+//   } else {
+//     next() // 繼續導航
+//   }
+// })
 
 export default router
