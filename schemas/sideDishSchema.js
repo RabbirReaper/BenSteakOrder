7eb913dac368f14a42cb0c4af0a3fb83ab077f9c
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-02-11 22:21:15 +0800

    重命名附餐模式為 ElseDish

--- schemas/sideDishSchema.js
+++ schemas/sideDishSchema.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-const sideDishSchema = new mongoose.Schema({
+const elseDishSchema = new mongoose.Schema({
   name: { type: String, required: true }, // 附餐名稱
   description: { type: String }, // 餐點說明
   price: { type: Number, required: true }, // 餐點價格
-});
+}, { timestamps: true });
 
-export const SideDish = mongoose.model('SideDish', sideDishSchema);
+export default mongoose.model('ElseDish', elseDishSchema);
