1baadfb46e66d0fa6ca5db0ecbe5557f071396c9
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-02-11 22:21:38 +0800

    與上次提交相同

--- schemas/sideDishSchema.js
+++ schemas/sideDishSchema.js
@@ -4,6 +4,6 @@ const elseDishSchema = new mongoose.Schema({
   name: { type: String, required: true }, // 附餐名稱
   description: { type: String }, // 餐點說明
   price: { type: Number, required: true }, // 餐點價格
-}, { timestamps: true });
+});
 
 export default mongoose.model('ElseDish', elseDishSchema);
