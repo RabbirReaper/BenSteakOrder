bdf366c23daa818c01e98bf19709727f7154dbbd
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-02-15 13:04:28 +0800

    重構菜單架構，將 items 改為 list，並新增 categoryName 屬性以改善資料結構

--- schemas/menuSchema.js
+++ schemas/menuSchema.js
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose';
 
 const menuSchema = new mongoose.Schema({
-  category: { type: String, required: true }, // 菜單類別（例如：主餐、附餐、食材加點）
-  items: [
+  list: [
     {
-      itemId: { type: mongoose.Schema.Types.ObjectId, required: true, refPath: 'itemModel' }, // 對應的餐點 ID
-      itemModel: { type: String, required: true }, // 餐點模型名稱（例如：MainDish、SideDish）
-      order: { type: Number, default: 0 }, // 顯示順序
+      categoryName: { type: String, required: true }, // 菜單類別（例如：主餐、附餐、食材加點）
+      items: [
+        {
+          itemModel: { type: String, required: true }, // 餐點模型名稱（例如：MainDish、SideDish）
+          itemId: { type: mongoose.Schema.Types.ObjectId, required: true, refPath: 'itemModel' }, // 對應的餐點 ID
+          order: { type: Number, default: 0 }, // 顯示順序
+        }
+      ]
     }
-  ],
+  ]
 });
 
 export default mongoose.model('Menu', menuSchema);
