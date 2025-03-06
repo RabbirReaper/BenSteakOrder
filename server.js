b255b849d0017b3e2fd8c7c358484606aee3d7de
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-03-06 15:15:14 +0800

    調整 server.js 中的請求大小限制，將 JSON 和 URL 編碼的最大限制從 10MB 降至 5MB

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ dotenv.config()
 const app = express()
 const port = 8700
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
