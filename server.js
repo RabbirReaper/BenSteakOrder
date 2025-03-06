9b3fa36600ddf021be2c802a4244a079939f0989
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-03-06 15:18:40 +0800

    調整 server.js 中的埠號設定，改為使用環境變數 PORT，並更新 session 密鑰的設定方式

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config()
 const app = express()
-const port = 8700
+const port = process.env.PORT || 8700
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
@@ -26,7 +26,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
-  secret: 'your-secret-key',
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
   resave: false,
   saveUninitialized: false,
   cookie: {
