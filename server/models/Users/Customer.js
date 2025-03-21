cbd310102c015f489ab18fc7b9eb7b9ab2c34459
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-03-21 21:24:48 +0800

    新增顧客模型屬性：生日、地址及性別

--- server/models/Users/Customer.js
+++ server/models/Users/Customer.js
@@ -7,6 +7,9 @@ const customerSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  birthday: { type: Date }, // 顧客生日
+  address: { type: String }, // 顧客地址
+  gender: { type: String, enum: ['男性', '女性', '其他'] }, // 顧客性別
   coupons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Coupon' }], // 顧客擁有的優惠券
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }], // 顧客的訂單
 }, { timestamps: true });
