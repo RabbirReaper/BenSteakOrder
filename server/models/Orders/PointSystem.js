01acc8f7aaf31c9a79dd7255004aeb9460d53556
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-03-23 11:47:14 +0800

    新增點數系統模型，定義轉換公式及啟用狀態

---
+++ server/models/Orders/PointSystem.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const pointSystemSchema = new mongoose.Schema({
+  name: { type: String, required: true }, // 例如 "標準轉換" 或 "雙倍點數日"
+  minAmount: { type: Number, required: true }, // 最低消費金額
+  formula: { type: String, required: true }, // 儲存公式，如 "0.003125*(x-300)**1.1+1"
+  desciption: { type: String }, // 描述
+  active: { type: Boolean, default: false }, // 是否啟用
+});
+
+export default mongoose.model("PointSystem", pointSystemSchema);
