455da0c739652ff8d4b12c5d178402cc4aa07ce8
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-03-30 11:22:08 +0800

    修改一些註解

--- server/models/Orders/PointSystem.js
+++ server/models/Orders/PointSystem.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const pointSystemSchema = new mongoose.Schema({
   name: { type: String, required: true }, // 例如 "標準轉換" 或 "雙倍點數日"
   minAmount: { type: Number, required: true }, // 最低消費金額
-  formula: { type: String, required: true }, // 儲存公式，如 "0.003125*(x-300)**1.1+1"
+  formula: { type: String, required: true }, // 儲存公式，如 "floor(x/300)"
   desciption: { type: String }, // 描述
   active: { type: Boolean, default: false }, // 是否啟用
 });
